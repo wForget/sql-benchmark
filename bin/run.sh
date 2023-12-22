59b33fb616a311d15d958c08bba73393d0de28ba
wforget <643348094@qq.com>
2023-12-22 11:36:19 +0800

    Expose REPORT_NAME

--- bin/run.sh
+++ bin/run.sh
@@ -8,7 +8,7 @@ cmd_args=${@:2}
 current_dir=`dirname "$0"`
 work_dir=`cd "$current_dir";cd ../; pwd`
 
-sql_dir=${SQL_DIR:-${work_dir}/sql/tpcds}
+SQL_DIR=${SQL_DIR:-${work_dir}/sql/tpcds/kyuubi_tpcds_3_2}
 
 . ${work_dir}/bin/functions/workload_functions.sh
 
@@ -16,14 +16,14 @@ checkCMD bc
 checkCMD $cmd
 
 export REPORT_DIR=${REPORT_DIR:-${work_dir}/report}
-export REPORT_NAME="sql_benchmark_$(timestamp).report"
+export REPORT_NAME=${REPORT_NAME:-"sql_benchmark_$(timestamp).report"}
 export REPORT_COLUMN_FORMATS="%-12s %-10s %-8s %-20s\n"
 
 if [ ! -e ${REPORT_DIR} ] ; then
     mkdir ${REPORT_DIR}
 fi
 
-sql_files=$(ls $sql_dir/*.sql)
+sql_files=$(ls $SQL_DIR/*.sql)
 for file in $sql_files
 do
     start=$(timestamp)
